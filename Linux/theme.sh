ecc1632ac306078daa17de21d4c3ce4bba4602d9
luoshi006 <luoshi006@gmail.com>
2017-09-13 10:50:18 +0800

    ADD my ubuntu shell files.

---
+++ Linux/theme.sh
@@ -0,0 +1,13 @@
+# theme for ubuntu 16.04
+
+sudo add-apt-repository ppa:noobslab/themes -y
+sudo apt-get update
+sudo apt-get install flatabulous-theme -y
+
+sudo add-apt-repository ppa:noobslab/icons -y
+sudo apt-get update
+sudo apt-get install ultra-flat-icons -y
+
+sudo add-apt-repository ppa:papirus/papirus -y
+sudo apt-get update
+sudo apt-get install papirus-icon-theme -y
